8c15b3471383544f5da2c8cb354a265abf7e63e0
LMN200 <lmnelson2580@gmail.com>
2022-11-02 22:06:21 +0000

    Final

--- i_webscraping_GUI/model.py
+++ i_webscraping_GUI/model.py
@@ -1,191 +1,176 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep 13 15:05:44 2022
-
 @author: gylmn
 """
+# Import packages
 import random
-import operator
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot
-import matplotlib.animation 
-import agentframework
+import agentframework24
 import csv
+import matplotlib.animation
+import matplotlib.backends.backend_tkagg as tkagg
+import matplotlib; matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
 import tkinter
 import requests
 import bs4
 
-# //ds.leeds.ac.uk/student/student14/gylmn/"Leeds Module"/Practicals
-random.seed(2)
+plt.interactive(False)
 
-r = requests.get('http://www.geog.leeds.ac.uk/courses/computing/practicals/python/agent-framework/part9/data.html', verify=False)
+# set up webscrapping using beautiful soup - retrieve coordinates from webpage
+r = requests.get('http://www.geog.leeds.ac.uk/courses/computing/practicals/python/agent-framework/part9/data.html',
+                 verify=False)
 content = r.text
 soup = bs4.BeautifulSoup(content, 'html.parser')
-td_ys = soup.find_all(attrs={"class" : "y"})
-td_xs = soup.find_all(attrs={"class" : "x"})
+td_ys = soup.find_all(attrs={"class": "y"})
+td_xs = soup.find_all(attrs={"class": "x"})
 print(td_ys)
 print(td_xs)
 
+# calculate/define calculation for distance
+def distance_between(a, b):
+    x_distance = abs(a.x - b.x)  # default
+    y_distance = abs(a.y - b.y)  # default
 
-#create the environment
-environment = []
+    # Due to torus, distance can be calculated in both directions to
+    # ensure smallest distance
+    if x_distance > 50:
+        x_distance = 100 - x_distance
+
+    if y_distance > 50:
+        y_distance = 100 - y_distance
+
+    round((float((x_distance ** 2) + (y_distance) ** 2)) ** 0.5, 2)
+
+# create the environment - open list of lists
 with open('in.txt', newline='') as f:
     dataset = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-    for row in dataset:
-        rowlist = []
-        for value in row:
-            rowlist.append(value)
-            # print(value)
-        environment.append(rowlist)
+    environment = [line for line in dataset]
 
-# Test environment has loaded
-# matplotlib.pyplot.imshow(environment)
-# matplotlib.pyplot.show()
+# # Test environment has loaded
+#plt.imshow(environment)
 
 
-def distance_between(a, b):
-    """
-    Calculates and returned the distance between a and b
-
-    Parameters
-    ----------
-    a : TYPE
-        DESCRIPTION.
-    b : TYPE
-        DESCRIPTION.
-
-    Returns
-    -------
-    TYPE
-        DESCRIPTION.
-
-    """
-    return (((a.x - b.x)**2) + ((a.y - b.y)**2))**0.5
-      
+# create agents list
 agents = []
-num_of_agents = 101
+
+# set limits/ assign values for all functions and classes
+num_of_agents = 10
 num_of_iterations = 100
 neighbourhood = 20
+breeding_distance = 10
+max_number_of_rams = 2
 
+# create figure with size limits
+fig = plt.figure(figsize=(7, 7))
+ax = fig.add_axes([0, 0, 1, 1])
+
+# plot environment
+im = ax.imshow(environment)
 
 # Initialise agents
 for i in range(num_of_agents):
-    #agents.append([random.randint(0,99), random.randint(0,99)])
-    #agents.append(agentframework.Agent(i, environment, agents))
-    if (i>= len(td_ys)):
-        y = random.randint(0,99)
-        x = random.randint(0,99)
+    ram = True
+    if i > max_number_of_rams-1:
+        ram = False
+
+    if (i >= len(td_ys)):
+        y = random.randint(0, 99)
+        x = random.randint(0, 99)
     else:
         y = int(td_ys[i].text)
         x = int(td_xs[i].text)
-    agents.append(agentframework.Agent(i, environment, agents, y, x))
+        print(f'({x}, {y})')
+    agents.append(agentframework24.Agent(i, environment, agents, ram=ram, x=x, y=y))
 
-# print agents
-for i in range(num_of_agents):
-    #print(agents[i].x, agents[i].y)
-    print(agents[i])
+# print("Print out agents[1] from agents[0] as a test")
 
-print("Print out agents[1] from agents[0] as a test")
-[print(agents[0].agents[1])]
+carry_on = True
 
-carry_on = True	
-
-# Move agents
-def update(frame_number):
-    
+# Make animation containing move, eat, share, breed
+def animate(frame_number):
     print("iteration", frame_number)
     fig.clear()
     global carry_on
-    #random.shuffle(agents) # shuffle agents
-    # Move agents first then eat and share
-    for i in range(num_of_agents):
-        agents[i].move()
-        # # create a torus allowing agents to leave top and appear at the bottom,
-        # # left to right etc.
-    for i in range(num_of_agents):
+
+    # Move agents first then eat, share and breed
+    for i in range(len(agents)):
+        agents[i].move()  # agents can move in all directions across torus
+
+    for i in range(len(agents)):
         agents[i].eat()
-        agents[i].share_with_neighbours(neighbourhood)
-    
-    # # Stopping condition - Random
-    # if random.random() < 0.1:
-    #     carry_on = False
-    #     print("stopping condition")
-    
-    
-    # Stopping Condition - All agents food store > 20
+        agents[i].share_with_neighbours(neighbourhood, breeding_distance)
+        agents[i].age += 1
+
+    # Stopping Condition - 90 counts yeilds roughly 80 agents (depending on last
+    # breeding cycle number of lambs / distance of rams from ewes) + inital 10 agents
     stop = False
     count = 0
-    for i in range(num_of_agents):
-        if (agents[i].store > 80):
-            count = count + 1
-    if count == len(agents):
-        print("stopping condition at frame", frame_number)
-        carry_on = False
-    
+    for i in range(len(agents)):
+        if len(agents) >= 10:
+            count += 1   
+        if count == 90:  # count can be increased here to run model for longer
+            print("stopping condition at frame", frame_number)
+            print(len(agents))
+            carry_on = False
+
     # print("After Move")
     #  # print the agents
     # for i in range(num_of_agents):
     #     #print(agents[i].x, agents[i].y)
     #     print(agents[i])
-        
-        
-    # Plot agents
-    matplotlib.pyplot.ylim(0, 99)
-    matplotlib.pyplot.xlim(0, 99)
-    matplotlib.pyplot.imshow(environment)
-    for i in range(num_of_agents):
-        matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
-    # matplotlib.pyplot.show()
+
+    # Plot agents onto figure - blue for rams, red for ewes (all other agents)
+    plt.ylim(0, 99)
+    plt.xlim(0, 99)
+    plt.imshow(environment)
+
+    for i in range(len(agents)):
+        if agents[i].ram:
+            c = "blue"
+        else:
+            c = "red"
+        plt.scatter(agents[i].x, agents[i].y, c=c)
     canvas.draw()
+    # exit(9)
+    return
 
-
-def gen_function(b = [0]):
+# use gen_function to start at frame 0 and add one each time to know what
+# framenumber model finishes on
+def gen_function(b=[0]):
     a = 0
-    global carry_on #Not actually needed as we're not assigning, but clearer
-    while (a < 10) & (carry_on) :
-        yield a			# Returns control and waits next call.
+    global carry_on
+    while (a < 1000) & (carry_on):
+        yield a  # Returns control and waits next call.
+
         a = a + 1
 
-# animation = matplotlib.animation.FuncAnimation(
-    # fig, update, interval=1, repeat=False, frames=num_of_iterations)
-# animation = matplotlib.animation.FuncAnimation(fig, update, frames=gen_function, repeat=False)
-
+# run the animation
 def run():
-    animation = matplotlib.animation.FuncAnimation(fig, update, frames=gen_function, repeat=False)
+    print('we got into run')
+    anim = matplotlib.animation.FuncAnimation(fig, animate, frames=gen_function, repeat=False)
+    print('we got just before draw')
     canvas.draw()
-
-# Animation
-fig = matplotlib.pyplot.figure(figsize=(7, 7))
-ax = fig.add_axes([0, 0, 1, 1])
+    return
 
 # Create GUI
-root = tkinter.Tk() 
+root = tkinter.Tk()
 root.wm_title("Model")
-canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
+
+canvas = tkagg.FigureCanvasTkAgg(fig, master=root)
 canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-menu = tkinter.Menu(root) # set up menu
+
+menu = tkinter.Menu(root)  # set up menu
 root.config(menu=menu)
+
 model_menu = tkinter.Menu(menu)
 menu.add_cascade(label="Model", menu=model_menu)
-model_menu.add_command(label="Run model", command=run) 
+
+model_menu.add_command(label="Run model", command=run())
+print('we got this far')
+canvas.draw()
 
 tkinter.mainloop()
 
-for j in range(num_of_agents):
-# Move agents
-    for i in range(j + 1 , num_of_agents):
-        distance = distance_between(agents[j], agents[i])
-        # print(distance)
-
-tkinter.mainloop()
-
-# press enter to stop kernel
-# input("Press enter to exit ;)")
-
-
-
-
-
-
+exit(9)
 
